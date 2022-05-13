5da9896d532cdefa51b830c71bbe221a324ad5b7
maxab123 <82547160+maxab123@users.noreply.github.com>
2022-05-14 01:47:49 +0200

    Add files via upload
    File python_book_intro from local directory Uploaded.

---
+++ 1_python_book_intro.py
@@ -0,0 +1,29 @@
+
+
+#-------------------STRINGS----------------------
+long_string = '''This is a multiple line string a
+    This is really good'''
+#print(long_string)
+long_string = """
+    This is a multiple line string f
+    This is also very good."""
+'''what is happening in this place'''
+print(long_string)
+my_long_string = "This is a very long string that I want to \
+write out on a single line, since it is too long."
+
+
+#-----------------------INDEXING----------------
+print(my_long_string)
+flavour = "Birthday Cake"
+print(flavour[0:3]) #from 0 index to 3 but not including 3(that is 2)
+this = "Yeah"
+that = "Man, I appreciate"
+print(this, that)
+word = "bazinga"
+print(word[2:-1])
+
+
+user_input = input("What are you up to :)>>>")
+print("Just a Reminder you are up to:", user_input)
+
